f85b16a895a8d9f184c24d83a3cc8b5ac662aabc
Frank Pagan <frank@cocreate.app>
2022-09-29 19:29:11 +0000

    fix: config renameed to CoCreateConfig

--- src/apikey.js
+++ src/apikey.js
@@ -1,11 +1,11 @@
              
-    var config = {                 
+    var CoCreateConfig = {                 
         apiKey: '2061acef-0451-4545-f754-60cf8160',                 
         organization_id: '5ff747727005da1c272740ab',                 
         host: 'wss://general.cocreate.app'             
     }         
     
-    // var config = {
+    // var CoCreateConfig = {
     //     apiKey: 'c2b08663-06e3-440c-ef6f-13978b42883a',
     //     organization_id: '5de0387b12e200ea63204d6c',
     //     host: 'wss://server.cocreate.app:8088'
