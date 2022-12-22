b81a7c144d30784a5b9cfc57811fbaa146801a03
Frank Pagan <frank@cocreate.app>
2022-12-22 15:06:36 +0000

    fix: update cdn

--- src/apikey.js
+++ src/apikey.js
@@ -5,8 +5,3 @@
         host: 'wss://general.cocreate.app'             
     }         
     
-    // var CoCreateConfig = {
-    //     apiKey: 'c2b08663-06e3-440c-ef6f-13978b42883a',
-    //     organization_id: '5ff747727005da1c272740ab',
-    //     host: 'wss://server.cocreate.app:8088'
-    //   }
