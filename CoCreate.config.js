67606ec9b8fb6823ca46a67fc5ec466d498e1b05
frankpagan <frank@cocreate.app>
2023-08-08 14:50:12 -0600

    feat: renamed CRUD attributes and params
    - `collection` to `array`
    - `name` to `key`
    - `document_id` to `object`

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -8,8 +8,8 @@ module.exports = {
             "exclude": [
                 "demos"
             ],
-            "collection": "files",
-            "document": {
+            "array": "files",
+            "object": {
                 "name": "{{name}}",
                 "src": "{{source}}",
                 "host": [
@@ -26,15 +26,15 @@ module.exports = {
     ],
     "sources": [
         {
-            "collection": "demos",
-            "document": {
+            "array": "demos",
+            "object": {
                 "_id": "619d495ca8b6b4001a9e6127",
                 "collaboration-demo": "{{./src/demos/collaboration.html}}"
             }
         },
         {
-            "collection": "demos",
-            "document": {
+            "array": "demos",
+            "object": {
                 "_id": "639b325f6f44f0f162f0bcbf",
                 "demo-code": "{{./src/demos/demo.html}}"
             }
