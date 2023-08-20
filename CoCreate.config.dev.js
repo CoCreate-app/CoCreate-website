98e400060c2a18e611c420fbe35f8c6064519397
frankpagan <frank@cocreate.app>
2023-08-20 15:18:18 -0600

    add CoCreate.config for dev environment

---
+++ CoCreate.config.dev.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "sources": [
+        {
+            "array": "files",
+            "object": {
+                "name": "index.html",
+                "src": "{{./src/index.html}}",
+                "host": [
+                    "*",
+                    "general.cocreate.app"
+                ],
+                "directory": "/",
+                "path": "/index.html",
+                "content-type": "text/html",
+                "public": "true",
+                "_id": "645be281109a1c102bbd98c3"
+            }
+        }
+    ]
+};
