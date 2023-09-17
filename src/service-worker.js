757930ef6dbafac6a3e725c5c2c72a0b75c50cf4
frankpagan <frank@cocreate.app>
2023-09-17 10:52:38 -0600

    fix: removed code related to storage false and readFIle() as cache now updates dynaically using crud and socket listener

--- src/service-worker.js
+++ src/service-worker.js
@@ -15,10 +15,12 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  ********************************************************************************/
 
-// Commercial Licensing Information:
-// For commercial use of this software without the copyleft provisions of the AGPLv3,
-// you must obtain a commercial license from CoCreate LLC.
-// For details, visit <https://cocreate.app/licenses/> or contact us at sales@cocreate.app.
+/**
+ * Commercial Licensing Information:
+ * For commercial use of this software without the copyleft provisions of the AGPLv3,
+ * you must obtain a commercial license from CoCreate LLC.
+ * For details, visit <https://cocreate.app/licenses/> or contact us at sales@cocreate.app.
+ */
 
 const cacheName = "dynamic-v2";
 let organization_id = ""
@@ -39,41 +41,6 @@ self.addEventListener("activate", async (e) => {
 
 self.addEventListener("fetch", async (e) => {
     if (!(e.request.url.indexOf('http') === 0) || e.request.method === 'POST') return;
-
-    if (!storage) {
-        console.log('storage:', storage);
-
-        const url = new URL(e.request.url);
-        const hostname = url.host;
-        const pathname = url.pathname;
-
-        let file = await readFile({
-            database: '5ff747727005da1c272740ab',
-            array: 'files',
-            filter: {
-                path: hostname,
-                host: '*'
-            }
-        });
-
-        if (file) {
-            console.log('file:', file);
-            console.log('Pathname:', pathname);
-            console.log('file', file)
-        }
-
-        if (file && file.object && file.object[0]) {
-            file = file.object[0]
-
-            const modifiedRequest = new Request(e.request, {
-                headers: new Headers({
-                    'File-Content': file.src,
-                    'Content-Type': file['content-type']
-                })
-            });
-            e.request = modifiedRequest
-        }
-    }
 
     e.respondWith(
         caches
@@ -131,56 +98,6 @@ self.addEventListener("fetch", async (e) => {
             })
     );
 });
-
-
-function readFile(data) {
-    return new Promise((resolve) => {
-        const request = indexedDB.open(data.database);
-
-        request.onsuccess = function () {
-            const db = request.result
-            console.log('readFile', data)
-            try {
-                const transaction = db.transaction(data.array, "readonly");
-                const objectStore = transaction.objectStore(data.array);
-                const cursorRequest = objectStore.openCursor();
-
-                cursorRequest.onsuccess = function () {
-                    const cursor = cursorRequest.result;
-                    if (cursor) {
-                        const file = cursor.value;
-                        if (!file || !file.src || !file.path || !file.host)
-                            cursor.continue();
-                        else if (file.path !== data.filter.path || !file.host.includes(data.filter.host))
-                            cursor.continue();
-                        else {
-                            db.close()
-                            return resolve(file);
-                        }
-                    } else {
-                        // Resolve the Promise when cursor is finished
-                        db.close()
-                        resolve();
-                    }
-                };
-
-                cursorRequest.onerror = function () {
-                    console.error("Cursor error:", cursorRequest.error);
-                    db.close()
-                    resolve();
-                };
-            } catch (error) {
-
-            }
-
-        };
-
-        request.onerror = function (event) {
-            reject(event.target.error);
-        };
-
-    })
-}
 
 self.addEventListener('message', function (event) {
     if (event.data === 'getOrganization')
