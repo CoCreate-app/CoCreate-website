a5fc15282d367a6610fc5d31d2886d27c7999a9b
frankpagan <frank@cocreate.app>
2023-09-18 18:45:07 -0600

    removed apikey.js

--- src/apikey.js
+++
@@ -1,7 +0,0 @@
-             
-    var CoCreateConfig = {                 
-        key: '2061acef-0451-4545-f754-60cf8160',                 
-        organization_id: '5ff747727005da1c272740ab',                 
-        // host: 'wss://general.cocreate.app'             
-    }         
-    
