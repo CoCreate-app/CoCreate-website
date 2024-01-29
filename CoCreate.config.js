75d385bc13346cc183a2a47cf09e64862f04e0d3
frankpagan <frank@cocreate.app>
2024-01-29 12:36:54 -0500

    fix: ignor .txt

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -9,8 +9,7 @@ module.exports = {
         {
             "entry": "./src",
             "exclude": [
-                "demos",
-                ".txt"
+                "demos"
             ],
             "array": "files",
             "object": {
