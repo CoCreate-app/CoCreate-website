976be26299cff37c62438c70cc2f213941a7d4df
frankpagan <frank@cocreate.app>
2024-11-29 17:13:28 -0600

    fix: remove exclude demos from config

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -9,9 +9,6 @@ module.exports = {
     "directories": [
         {
             "entry": "./src",
-            "exclude": [
-                "demos"
-            ],
             "array": "files",
             "object": {
                 "name": "{{name}}",
