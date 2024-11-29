53f1927bdec36536933ec6ab8900deefd6f5e5eb
frankpagan <frank@cocreate.app>
2024-11-29 17:20:28 -0600

    fix: formating

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -1,46 +1,42 @@
 module.exports = {
-    "host": {
-        "$branch": {
-            "master": "wss://cocreate.app",
-            "dev": "wss://dev.cocreate.app",
-            "test": "wss://test.cocreate.app"
-        }
+  host: {
+    $branch: {
+      master: "wss://cocreate.app",
+      dev: "wss://dev.cocreate.app",
+      test: "wss://test.cocreate.app",
     },
-    "directories": [
-        {
-            "entry": "./src",
-            "exclude": [
-                "demos"
-            ],
-            "array": "files",
-            "object": {
-                "name": "{{name}}",
-                "src": "{{source}}",
-                "host": [
-                    "*"
-                ],
-                "directory": "/",
-                "path": "{{path}}",
-                "pathname": "{{pathname}}",
-                "content-type": "{{content-type}}",
-                "public": "true"
-            }
-        }
-    ],
-    "sources": [
-        {
-            "array": "demos",
-            "object": {
-                "_id": "619d495ca8b6b4001a9e6127",
-                "collaboration-demo": "{{./src/demos/collaboration.html}}"
-            }
-        },
-        {
-            "array": "demos",
-            "object": {
-                "_id": "639b325f6f44f0f162f0bcbf",
-                "demo-code": "{{./src/demos/demo.html}}"
-            }
-        }
-    ]
+  },
+  directories: [
+    {
+      entry: "./src",
+      exclude: ["demos"],
+      array: "files",
+      object: {
+        name: "{{name}}",
+        src: "{{source}}",
+        host: ["*"],
+        directory: "/",
+        path: "{{path}}",
+        pathname: "{{pathname}}",
+        "content-type": "{{content-type}}",
+        public: "true",
+      },
+    },
+  ],
+  sources: [
+    {
+      array: "demos",
+      object: {
+        _id: "619d495ca8b6b4001a9e6127",
+        "collaboration-demo": "{{./src/demos/collaboration.html}}",
+      },
+    },
+    {
+      array: "demos",
+      object: {
+        _id: "639b325f6f44f0f162f0bcbf",
+        "demo-code": "{{./src/demos/demo.html}}",
+      },
+    },
+  ],
 };
